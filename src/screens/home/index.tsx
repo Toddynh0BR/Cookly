71a337962cf045446901f9e1c4b96ee6b2f4f166
Toddynh0BR <galaxyplay41@gmail.com>
2026-03-11 20:15:06 -0300

    Exclusão de rota teste

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -11,6 +11,8 @@ import { useAuth } from "../../hooks/auth";
 
 import { Header } from "../../components/header";
 
+import { HeartIcon } from 'phosphor-react-native';
+
 export function Home() {
     const navigation = useNavigation();//navegação entre telas
     const { logout } = useAuth();
@@ -19,32 +21,71 @@ export function Home() {
       {
         id: 1,
         name: "Frango Assado",
-        image: "https://www.receiteria.com.br/wp-content/uploads/receitas-de-frango-assado-1.jpg"
+        img: "https://www.receiteria.com.br/wp-content/uploads/receitas-de-frango-assado-1.jpg"
       },
       {
         id: 2,
         name: "Bolo de Chocolate",
-        image: "https://www.receiteria.com.br/wp-content/uploads/bolo-de-chocolate-rapido-e-molhadinho-capa.png"
+        img: "https://www.receiteria.com.br/wp-content/uploads/bolo-de-chocolate-rapido-e-molhadinho-capa.png"
       },
       {
         id: 3,
         name: 'Salada de Frutas',
-        image: 'https://www.receiteria.com.br/wp-content/uploads/salada-de-frutas-1.jpg'
+        img: 'https://www.receiteria.com.br/wp-content/uploads/salada-de-frutas-1.jpg'
       },
       {
         id: 4,
         name: 'Hamburguer Artesanal',
-        image: 'https://www.receiteria.com.br/wp-content/uploads/hamburguer-artesanal-facil-e-rapido.jpg'
+        img: 'https://www.receiteria.com.br/wp-content/uploads/hamburguer-artesanal-facil-e-rapido.jpg'
       },
       {
         id: 5,
         name: 'Pudim de Leite',
-        image: 'https://www.receiteria.com.br/wp-content/uploads/pudim-de-leite-condensado-capa.jpeg'
+        img: 'https://www.receiteria.com.br/wp-content/uploads/pudim-de-leite-condensado-capa.jpeg'
       },
             {
         id: 6,
         name: 'Panqueca Americana',
-        image: 'https://www.receiteria.com.br/wp-content/uploads/panqueca-americana.jpeg'
+        img: 'https://www.receiteria.com.br/wp-content/uploads/panqueca-americana.jpeg'
+      },
+    ]);
+
+    const [PopularRecipes, setPopularRecipes] = useState([
+      {
+        id: 1,
+        name: "Frango Assado",
+        img: "https://www.receiteria.com.br/wp-content/uploads/receitas-de-frango-assado-1.jpg",
+        time: '140-180min'
+      },
+      {
+        id: 2,
+        name: "Bolo de Chocolate",
+        img: "https://www.receiteria.com.br/wp-content/uploads/bolo-de-chocolate-rapido-e-molhadinho-capa.png",
+        time: '140-180min'
+      },
+      {
+        id: 3,
+        name: 'Salada de Frutas',
+        img: 'https://www.receiteria.com.br/wp-content/uploads/salada-de-frutas-1.jpg',
+        time: '140-180min'
+      },
+      {
+        id: 4,
+        name: 'Hamburguer Artesanal',
+        img: 'https://www.receiteria.com.br/wp-content/uploads/hamburguer-artesanal-facil-e-rapido.jpg',
+        time: '140-180min'
+      },
+      {
+        id: 5,
+        name: 'Pudim de Leite',
+        img: 'https://www.receiteria.com.br/wp-content/uploads/pudim-de-leite-condensado-capa.jpeg',
+        time: '140-180min'
+      },
+            {
+        id: 6,
+        name: 'Panqueca Americana',
+        img: 'https://www.receiteria.com.br/wp-content/uploads/panqueca-americana.jpeg',
+        time: '140-180min'
       },
     ]);
 
@@ -57,22 +98,67 @@ export function Home() {
       <S.Content>
        <S.DayRecipes>
         <S.DRTitle>
-          Receitas do dia
+         Receitas do dia
         </S.DRTitle>
         
         <S.DRRecipesList 
          data={DayRecipes}
          horizontal={true}
          keyExtractor={(item: any) => item.id}
-         ItemSeparatorComponent={() => <View style={{ width: 10 }} />}
           showsHorizontalScrollIndicator={false} 
          renderItem={({item}) => (
-          <S.DRRecipeCard source={{uri: item.image}}>
+          <S.DRRecipeCard>
+           <S.DRRecipeBack source={{uri: item.img}}>
+            <View style={{ width: '100%', height: '100%', backgroundColor: '#f15821', opacity: .2}}></View>
+           </S.DRRecipeBack>
 
+            <S.DRRecipeName>
+             {item.name}
+            </S.DRRecipeName>
           </S.DRRecipeCard>
          )}
         />
        </S.DayRecipes>
+
+       <S.PopularRecipes>
+        <View style={{ width: '100%', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
+         <S.PRTitle>
+          Receitas Populares
+         </S.PRTitle>
+
+         <TouchableOpacity>
+          <S.MoreText>
+           Ver todos
+          </S.MoreText>
+         </TouchableOpacity>
+        </View>
+
+        <S.PRRecipesList 
+         data={PopularRecipes}
+         horizontal={true}
+         keyExtractor={(item: any) => item.id}
+         showsHorizontalScrollIndicator={false} 
+         renderItem={({item})=> (
+          <S.PRRecipe> 
+           <S.PRImage source={{uri: item.img}} resizeMode="cover">
+            <S.PRTime>
+             <S.ClockI/>
+             <S.PRTimeText>
+              {item.time}
+             </S.PRTimeText>
+            </S.PRTime>
+           </S.PRImage>
+          
+           <View style={{ height: 24, width: '100%', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between'}}>
+            <S.PRText>
+              {item.name}
+            </S.PRText>
+            <HeartIcon size={24} color="#f15821" weight="fill"/>
+           </View>
+          </S.PRRecipe>
+         )}
+        />
+       </S.PopularRecipes>
       </S.Content>
 
      </S.Container>
--- src/screens/home/styles.ts
+++ src/screens/home/styles.ts
@@ -1,5 +1,5 @@
+import { ClockIcon } from 'phosphor-react-native';
 import styled from "styled-components/native";
-
 
 export const Container = styled.View`
   flex: 1;
@@ -19,12 +19,14 @@ width: 100%;
 export const DayRecipes = styled.View`
 height: fit-content;
 width: 100%;
+
 `
 
 export const DRTitle = styled.Text`
 font-family: 'lexend-Medium';
 font-size: 16px;
 color: ${(props) => props.theme.text1};
+padding: 0 12px;
 `
 
 export const DRRecipesList = styled.FlatList`
@@ -34,7 +36,18 @@ width: 100%;
 margin-top: 12px;
 `
 
-export const DRRecipeCard = styled.ImageBackground`
+export const DRRecipeBack = styled.ImageBackground`
+height: 120px;
+width: 120px;
+
+justify-content: center;
+align-items: center;
+display: flex;
+
+filter: blur(1);
+`
+
+export const DRRecipeCard = styled.TouchableOpacity`
 height: 120px;
 width: 120px;
 
@@ -43,11 +56,104 @@ align-items: center;
 display: flex;
 
 border-radius: 12px;
-filter: blur(1px);
+position: relative;
+overflow: hidden;
+margin-left: 5px;
+margin-right: 5px;
 `
 
 export const DRRecipeName = styled.Text`
 font-family: 'lexend-Bold';
 font-size: 16px;
 color: ${(props) => props.theme.text2};
+text-align: center;
+
+position: absolute;
 `
+
+export const PopularRecipes = styled.View`
+height: fit-content;
+width: 100%;
+
+margin-top: 45px;
+`
+
+export const PRTitle = styled.Text`
+font-family: 'lexend-Bold';
+font-size: 22px;
+color: ${(props) => props.theme.text1};
+padding: 0 12px;
+`
+
+export const MoreText = styled.Text`
+font-family: 'lexend-Bold';
+font-size: 12px;
+color: ${(props)=> props.theme.main};
+padding: 5px 12px 0px;
+`
+
+export const PRRecipesList = styled.FlatList`
+height: 1750px;
+width: 100%;
+
+margin-top: 12px;
+`
+
+export const PRRecipe = styled.TouchableOpacity`
+height: 211px;
+width: 175px;
+
+flex-direction: column;
+gap: 12px;
+margin-left: 5px;
+margin-right: 5px;
+`
+
+export const PRImage = styled.ImageBackground`
+height: 175px;
+width: 175px;
+
+position: relative;
+border-radius: 8px;
+overflow: hidden;
+`
+
+export const PRTime = styled.View`
+height: 30px;
+width: 92px;
+
+background-color: ${(props)=> props.theme.background};
+position: absolute;
+bottom: 0;
+left: 0;
+
+justify-content: center;
+align-items: center;
+flex-direction: row;
+gap: 5px;
+
+border-radius: 0 8px 0 0;
+`
+
+export const ClockI = styled(ClockIcon).attrs({
+  size: 14,
+})`
+  color: ${(props)=> props.theme.text1};
+`;
+
+export const PRTimeText = styled.Text`
+font-family: 'lexend-Bold';
+font-size: 10px;
+color: ${(props)=> props.theme.main};
+`
+
+export const PRText = styled.Text.attrs({
+  numberOfLines: 1,
+  ellipsizeMode: "tail",
+})`
+font-size: 'lexend';
+color: ${(props)=> props.theme.text1};
+font-size: 16px;
+width: 145px
+`
+
